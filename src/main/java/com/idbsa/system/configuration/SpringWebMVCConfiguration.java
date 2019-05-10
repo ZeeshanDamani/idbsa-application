138e45cde736c6f53dda284b73e23c48c9807771
Zeeshan Moiz Damani <zeeshan.damani@careem.com>
2019-05-10 23:15:42 +0500

    Added Database and Spring MVC Configuration

---
+++ src/main/java/com/idbsa/system/configuration/SpringWebMVCConfiguration.java
@@ -0,0 +1,92 @@
+package com.idbsa.system.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.MultipartFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+
+
+@Configuration
+public class SpringWebMVCConfiguration extends WebMvcConfigurerAdapter {
+
+//    @Autowired
+//    private CareemCloudRequestInterceptor careemCloudRequestInterceptor;
+//
+//    @Autowired
+//    private ExperimentInterceptor abTestInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(careemCloudRequestInterceptor);
+//        registry.addInterceptor(abTestInterceptor);
+//        super.addInterceptors(registry);
+//    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        super.addResourceHandlers(registry);
+    }
+
+    //Filters
+
+
+    @Bean(name = "encodingFilter")
+    public CharacterEncodingFilter getCharacterEncodingFilter(){
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(getCharacterEncodingFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean openEntityManagerFilterRegistrationBean() {
+        // Set upload filter
+        final MultipartFilter multipartFilter = new MultipartFilter();
+        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(multipartFilter);
+        filterRegistrationBean.addInitParameter("multipartResolverBeanName", "commonsMultipartResolver");
+
+        return filterRegistrationBean;
+    }
+
+
+    //Resolvers
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+    @Bean
+    public CommonsMultipartResolver commonsMultipartResolver() {
+        final CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setMaxUploadSize(20971520);
+        commonsMultipartResolver.setMaxInMemorySize(1048576);
+
+        return commonsMultipartResolver;
+    }
+}
